ead6d115f3221f77a52893378d6b72a3a7808bda
Mayur-Bhardwaj <mayurbhardwaj99@gmail.com>
2023-08-22 14:19:06 +0530

    TypeCasting

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+class Typecasting{
+    public static void main(String args[]){
+
+        // Type Casting --> 1. Automated TypeCasting  2. Forcefully By Programmer.
+        int i = 5;
+        double d = i;
+        System.out.println(d);
+
+        // Forcefully Typed Casting.
+
+        double myval = 12.01;
+        int myint = (int)myval;
+        System.out.println(myint);
+    }
+}
