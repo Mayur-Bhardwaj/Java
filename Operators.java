48524b2f0bac82ed49bc678c572c16db53d4d603
Mayur Bhardwaj <mayurbhardwaj99@gmail.com>
2023-08-28 17:37:59 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,13 @@
+// Operators  --> They are the symbols through which we can perform the operations like :
+// Arithmatic(+,-,*,/,%), Logical(&&, ||), Bitwise(&(bitwise AND),|(bitwise inclusive OR),^(bitwise exclusive OR), Ternary, Relation Operators.
+
+
+class Operator{
+    public static void main(String args[]){
+       var a=4; // assignment
+        System.out.println(a);
+        a++;
+         System.out.println(a);
+// If Else        
+    }
+} 
