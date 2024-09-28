28f9634d9669ac236d978f07ea7fd038704543eb
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-09-28 13:33:49 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        char data = 'A';
+        String name = "Java";
+        System.out.println(a);
+        System.out.println(data);
+        System.out.println(name);
+    }
+}
