65fa79098472b77d4d9413067a11c913ab44c3f7
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-12-20 20:26:58 +0530

    Data Types

---
+++ Java/DataTypes.java
@@ -0,0 +1,13 @@
+class DataTypes{
+    public static void main(String args[]){
+        boolean isValid = true;
+        byte marks = 90;
+        float pi = 3.14f;
+        float div = 15.0f/4.0f;
+        long views = 1000000;
+        char gender = 'M';
+        System.out.println(isValid + " "+ marks+" "+ pi+" "+ div+" "+views+" "+ gender);
+    }
+}
+
+//Output:-  true 90 3.14 3.75 1000000 M
