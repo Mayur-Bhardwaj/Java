30060c14583e69d2bfeae9f86ad6df57819b0c0c
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-12-21 23:56:41 +0530

    Update

--- Java/Primitive.java
+++ Java/Primitive.java
@@ -9,3 +9,15 @@ class Primitive{
 }
 
 //Output:-      Not Same
+
+
+class Test{
+    public static void main(String[] args) {
+        int x1 = 10;
+        Integer x2 = x1;    //AutoBoxing
+        int x3 = x2;        // Auto-Unboxing
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+    }
+}
