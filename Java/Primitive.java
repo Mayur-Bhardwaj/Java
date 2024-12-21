312d2205b6dcb4b3ed2fa318d828fa75b0fd0955
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-12-21 23:41:43 +0530

    Primitive

---
+++ Java/Primitive.java
@@ -0,0 +1,11 @@
+class Primitive{
+    public static void main(String[] args) {
+        Integer x1 = 400;Integer x2=400;
+        if(x1 == x2)
+            System.out.println("Same");
+        else
+            System.out.println("Not Same");
+    }
+}
+
+//Output:-      Not Same
